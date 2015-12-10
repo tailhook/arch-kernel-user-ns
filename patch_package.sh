ee5b7f3dc4834d4b19edb134586d49b587a43588
Paul Colomiets <paul@colomiets.name>
2015-12-10 22:41:17 +0200

    Made config for buildbot

---
+++ patch_package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -ex
+CONTRIBUTOR="Roman Rader <antigluk@gmail.com>"
+OVERLAYFS_PATCH="http:\\/\\/people.canonical.com\\/~apw\\/lp1377025-utopic\\/0001-UBUNTU-SAUCE-Overlayfs-allow-unprivileged-mounts.patch"
+
+sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config
+sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config.x86_64
+
+sed -i.bak "/source=/,/)/s/)$/\n$OVERLAYFS_PATCH)/" PKGBUILD
+sed -i "s/\# add upstream patch/\# add upstream patch\n  patch \-p1 \-i \"\$\{srcdir\}\/`basename $OVERLAYFS_PATCH`\"/" PKGBUILD
+sed -i 's/^pkgbase=linux.*$/pkgbase=linux-user-ns-enabled/' PKGBUILD
+sed -i "2i# Contributor: ${CONTRIBUTOR}" PKGBUILD
+sed -i '/pkgdesc=/{s/"$/ with CONFIG_USER_NS enabled"/}' PKGBUILD
+# Everything is owned by root
+sed -i '/chown/{s/^/#/}' PKGBUILD
+sed -i 's/cp -a /cp -a --no-preserve=ownership /' PKGBUILD
+sed -i 's/cp -al /cp -al --no-preserve=ownership /' PKGBUILD
+
+updpkgsums
+makepkg --source --skippgpcheck
