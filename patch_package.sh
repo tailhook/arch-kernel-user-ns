9d8a6b1357e95d34f6743458fc5cd62f91a49908
Paul Colomiets <paul@colomiets.name>
2017-03-15 23:57:27 +0200

    Fixes config name for i686 kernel

--- patch_package.sh
+++ patch_package.sh
@@ -2,7 +2,7 @@
 CONTRIBUTOR="Paul Colomiets <paul@colomiets.name>"
 OVERLAYFS_PATCH="http:\\/\\/people.canonical.com\\/~apw\\/lp1377025-utopic\\/0001-UBUNTU-SAUCE-Overlayfs-allow-unprivileged-mounts.patch"
 
-sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config
+sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config.i686
 sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config.x86_64
 
 sed -i.bak "/source=/,/)/s/)$/\n$OVERLAYFS_PATCH)/" PKGBUILD
