1af3d8c37256d379549217a14c0d007415b62d5b
Paul Colomiets <paul@colomiets.name>
2017-12-08 02:50:49 +0200

    Fix patch process

--- patch_package.sh
+++ patch_package.sh
@@ -2,8 +2,7 @@
 CONTRIBUTOR="Paul Colomiets <paul@colomiets.name>"
 OVERLAYFS_PATCH="http:\\/\\/people.canonical.com\\/~apw\\/lp1377025-utopic\\/0001-UBUNTU-SAUCE-Overlayfs-allow-unprivileged-mounts.patch"
 
-sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config.i686
-sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config.x86_64
+sed -i.bak "s/# CONFIG_USER_NS is not set/CONFIG_USER_NS=y/" config
 
 sed -i.bak "/source=/,/)/s/)$/\n$OVERLAYFS_PATCH)/" PKGBUILD
 sed -i "s/\# add upstream patch/\# add upstream patch\n  patch \-p1 \-i \"\$\{srcdir\}\/`basename $OVERLAYFS_PATCH`\"/" PKGBUILD
